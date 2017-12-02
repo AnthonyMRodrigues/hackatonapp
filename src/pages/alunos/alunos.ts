572e44a8b1e23050aca21f7bcbc3d770a7f0719c
Charley Oliveira <charleyoliveira@Charleys-MacBook-Pro.local>
2017-12-02 09:17:15 -0200

    Criando a pagina de alunos

--- src/pages/alunos/alunos.ts
+++ src/pages/alunos/alunos.ts
@@ -15,7 +15,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AlunosPage {
 
+    listaDeAlunos : any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.listaDeAlunos = [
+        {nome: "Teste 1", turma: "turma 1", img: "http://lorempixel.com/400/200"},
+        {nome: "Teste 2", turma: "turma 2", img: "http://lorempixel.com/400/200"},
+        {nome: "Teste 3", turma: "turma 3", img: "http://lorempixel.com/400/200"},
+        {nome: "Teste 4", turma: "turma 4", img: "http://lorempixel.com/400/200"},
+        {nome: "Teste 5", turma: "turma 5", img: "http://lorempixel.com/400/200"}
+    ];
   }
 
   ionViewDidLoad() {
