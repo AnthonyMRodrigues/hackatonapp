e929e1bb4adb1e5a5f77c323c02c8936b482d46e
Rodrigo Villardi Cortezi <rodrigovillardicortezi@Rodrigos-MacBook-Pro.local>
2017-12-02 16:11:07 -0200

    alunoView basico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AlunosPage } from '../pages/alunos/alunos';
 import { DashboardPage } from '../pages/dashboard/dashboard';
 import { TurmasPage } from '../pages/turmas/turmas';
 import { DadosDoAlunoPage } from '../pages/dados-do-aluno/dados-do-aluno';
+import { CalendarModule } from 'ionic3-calendar';
 
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -29,7 +30,8 @@ import { RequisitionsProvider } from '../providers/requisitions/requisitions';
   imports: [
     BrowserModule,
     HttpModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    CalendarModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
