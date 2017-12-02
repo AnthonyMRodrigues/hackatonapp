60175f4d72ed3a95a3ca20daa9c50a89cdea95cd
anthony.rodrigues <anthony.rodrigues@fidelize.com.br>
2017-12-02 09:22:31 -0200

    Login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,20 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+loginacess: any;
+login: any;
+password: String;
+passwordacess: any;
+permited: boolean;
   constructor(public navCtrl: NavController) {
 
   }
 
+  actionlogin()
+  {
+    this.loginacess = this.login;
+    this.passwordacess = this.password;
+    console.log(this.loginacess);
+    console.log(this.passwordacess);
+  }
 }
