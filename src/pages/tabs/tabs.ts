e0720e1a541fa35fbf49e5613560b791afad39b6
Rodrigo Villardi Cortezi <rodrigovillardicortezi@Rodrigos-MacBook-Pro.local>
2017-12-02 13:00:42 -0200

    Alterando tab para ter somento home(dashboard) e alunos.

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,16 +2,14 @@ import { Component } from '@angular/core';
 
 import { AlunosPage } from '../alunos/alunos';
 import { DashboardPage}  from '../dashboard/dashboard';
-import { DadosDoAlunoPage } from '../dados-do-aluno/dados-do-aluno';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = DashboardPage;
-  tab2Root = AlunosPage;
-  tab3Root = DadosDoAlunoPage;
+  homeTab = DashboardPage;
+  alunoTab = AlunosPage;
 
   constructor() {
 
