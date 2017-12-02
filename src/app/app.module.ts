783900cc0f5a8b515643f13428a674d882701eab
Charley Oliveira <charleyoliveira@Charleys-MacBook-Pro.local>
2017-12-02 08:38:49 -0200

    Subindo nav bar para todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { AlunosPage } from '../pages/alunos/alunos';
+import { DashboardPage } from '../pages/dashboard/dashboard';
+import { PerfilPage } from '../pages/perfil/perfil';
+import { TurmasPage } from '../pages/turmas/turmas';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +21,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    AlunosPage,
+    DashboardPage,
+    PerfilPage,
+    TurmasPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +37,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    AlunosPage,
+    DashboardPage,
+    PerfilPage,
+    TurmasPage
   ],
   providers: [
     StatusBar,
