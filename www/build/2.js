1116d4960a2b7ca66981f267bab653dcdd199ada
anthony.rodrigues <anthony.rodrigues@fidelize.com.br>
2017-12-03 07:35:17 -0200

    ionic build web

---
+++ www/build/2.js
@@ -0,0 +1,42 @@
+webpackJsonp([2],{
+
+/***/ 279:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DadosDoAlunoPageModule", function() { return DadosDoAlunoPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(16);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__dados_do_aluno__ = __webpack_require__(101);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var DadosDoAlunoPageModule = (function () {
+    function DadosDoAlunoPageModule() {
+    }
+    DadosDoAlunoPageModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["I" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_2__dados_do_aluno__["a" /* DadosDoAlunoPage */],
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__dados_do_aluno__["a" /* DadosDoAlunoPage */]),
+            ],
+        })
+    ], DadosDoAlunoPageModule);
+    return DadosDoAlunoPageModule;
+}());
+
+//# sourceMappingURL=dados-do-aluno.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=2.js.map
